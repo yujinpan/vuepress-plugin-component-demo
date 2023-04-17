51e8ca4917bf371514da0d31b6a711c1e68deef5
yujinpan <1192878390@qq.com>
2023-04-17 15:12:55 +0800

    feat: add vuepress-plugin-component-demo plugin

---
+++ src/demo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="demo custom-block tip" :style="{ borderColor: color }">
+    <p>{{ title }}</p>
+    <component :is="name" />
+    <details class="custom-block details">
+      <summary>Click me to show code</summary>
+      <p v-html="code"></p>
+    </details>
+
+  </div>
+</template>
+
+<script>
+import { CODES } from '@dynamic/demo-codes';
+
+export default {
+  props: {
+    name: String,
+    title: {
+      type: String,
+      default: '',
+    },
+    color: {
+      type: String,
+      default: 'gray',
+    },
+  },
+  computed: {
+    code() {
+      return CODES[this.name];
+    },
+  },
+};
+</script>
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import highlight from '@vuepress/markdown/lib/highlight';
+import fs from 'fs';
+import glob from 'glob';
+import path from 'path';
+
+const vuepressPluginComponentDemo = {
+  clientDynamicModules() {
+    const CODES = glob
+      .sync(path.resolve(__dirname, './components/*.vue'))
+      .reduce((prev, next) => {
+        const name = path.basename(next, path.extname(next));
+        const content = fs.readFileSync(next).toString();
+        const code = highlight(content, 'vue');
+
+        return {
+          ...prev,
+          [name]: code,
+        };
+      }, {});
+
+    return {
+      name: 'demo-codes.js',
+      content: `export const CODES = ${JSON.stringify(CODES)}`,
+    };
+  },
+  enhanceAppFiles: [path.resolve(__dirname, `./enhanceAppFile.js`)],
+};
+
+export default vuepressPluginComponentDemo;
